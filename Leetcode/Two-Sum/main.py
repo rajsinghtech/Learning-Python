eceb5809c6191b18cdbf2cfb463ae7dcb4d35a48
rajsinghtech <rajsingh@iastate.edu>
2023-05-01 20:04:00 -0500

    Added two sum

---
+++ Leetcode/Two-Sum/main.py
@@ -0,0 +1,25 @@
+nums = [2,7,11,15]
+target = 9
+
+# Old solution
+# def twoSum(nums, target):
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 return nums[i], nums[j]
+
+# Better solution
+def twoSum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return None
+ 
+
+            
+first, second = twoSum(nums, target)
+print(first, second)
+    
