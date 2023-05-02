03bde8757852dca60eab42a31be1af70cf5562b0
rajsinghtech <rajsingh@iastate.edu>
2023-05-01 20:04:31 -0500

    Added comment

--- Leetcode/Two-Sum/main.py
+++ Leetcode/Two-Sum/main.py
@@ -1,7 +1,7 @@
 nums = [2,7,11,15]
 target = 9
 
-# Old solution
+# Old solution (Brute force)
 # def twoSum(nums, target):
 #     for i in range(len(nums)):
 #         for j in range(i + 1, len(nums)):
